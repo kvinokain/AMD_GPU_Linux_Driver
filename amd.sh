925124021a9318f3181a88c2c42a1a4ed22576bf
kvinokain <147487388+kvinokain@users.noreply.github.com>
2023-10-10 11:23:23 +0300

    Update amd.sh

--- amd.sh
+++ amd.sh
@@ -2,43 +2,43 @@
 
 # Проверка наличия прав sudo
 if [ $(id -u) -ne 0 ]; then
-  echo "Этот скрипт требует прав суперпользователя (sudo)."
+  echo "You need sudo. Try sudo ./amd.sh (sudo)."
   exit 1
 fi
 
-# Включение строгого режима (-e)
+# Stopping the script in case of an error (-e)
 set -e
 
-# Путь к файлу для загрузки
+# New var
 download_file="amdgpu-install_5.5.50503-1_all.deb"
 
-# Проверка наличия файла перед загрузкой
+# Check file in dir
 if [ -e "$download_file" ]; then
-  echo "Файл $download_file уже существует. Пропуск этапа загрузки."
+  echo "File $download_file exist. Skip download."
 else
-  # Обновление списка пакетов и обновление системы
+  # Update && upgrade system
   apt update && apt upgrade -y
 
-  # Загрузка AMDGPU пакета
+  # Download .deb
   wget https://repo.radeon.com/amdgpu-install/23.10.3/ubuntu/focal/amdgpu-install_5.5.50503-1_all.deb
 fi
 
-# Установка AMDGPU пакета
+# Install deb
 sudo apt-get install ./$download_file
 
-# Повторное обновление списка пакетов
+# Reupdate
 sudo apt-get update
 
-# Установка AMDGPU с параметрами
+# Install GPU driver
 sudo amdgpu-install -y --accept-eula --usecase=opencl --opencl=rocr,legacy
 
-# Выключение строгого режима (-e), так как далее мы сами обрабатываем ошибку
+# Off set
 set +e
 
-# Проверка статуса установки
+# Check status
 if [ $? -eq 0 ]; then
-  echo "Установка AMDGPU завершена успешно."
+  echo "Installing successfully. Please reboot system"
 else
-  echo "Возникла ошибка при установке AMDGPU."
+  echo "Something wrong. Check terminal and log. Please report me about this"
 fi
 
